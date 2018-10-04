f14edcd83b6570f8ddf33ff9963ab4bed532545f
Brandon S <atomicbcs@gmail.com>
2018-10-03 20:01:06 -0400

    Created single-threaded brute forcer

---
+++ problem_4/ispc/brute_force.cpp
@@ -0,0 +1,68 @@
+#include "brute_force.h"
+
+unsigned char digest[MD5_DIGEST_LENGTH];
+unsigned char digest2[MD5_DIGEST_LENGTH];
+bool cracked = 0;
+
+int main(){
+    const char md5[]= "b1f4f9a523e36fd969f4573e25af4540"; 
+    str_to_md5(md5);
+    int lenght = 4;
+    char str[94];
+    gen_arr(str);
+    int n = sizeof str;
+    gen_strs(str, "", n, lenght);  //Note: this function works on all cases and not just the case above
+    return 0;
+
+}
+
+// The main recursive method to print all possible strings of length "length"
+void gen_strs(const char str[],string prefix,const int n, const int lenght){
+    if(cracked){
+        return;
+    } else if (lenght == 1){
+      for (int j = 0; j < n; j++){
+            // cout << prefix + str[j] << endl;
+            string pass = prefix + str[j];
+            
+            MD5((unsigned char*)pass.c_str(), pass.length(), (unsigned char*)&digest); 
+            if(memcmp(digest,digest2,MD5_DIGEST_LENGTH) == 0){
+                cout<< "PASSWORD: " << pass << endl;
+                cracked = 1;
+                return;
+            }
+            // md5_to_str(digest);
+        }
+    } else{
+            for (int i = 0; i < n; i++){
+              gen_strs(str, prefix + str[i], n, lenght - 1);
+            }
+        }
+}
+
+void gen_arr(char arr[]){
+    for(char i = 0;i < 95;i++){
+      arr[(int)i] = i + 32;
+    }
+}
+
+void str_to_md5(const char* str){
+    int n = strlen(str);
+    char * endptr;
+    for(int i = 0; i < n;i= i + 2){
+        char lett[3];
+        lett[3] = 0;
+        strncpy(lett,str + i,2);
+        long num = strtol(lett, &endptr, 16);
+        digest2[i / 2] = num;
+    }
+    printf("\n");
+}
+
+void md5_to_str(unsigned char* md) {
+    int i;
+    for(i=0; i <MD5_DIGEST_LENGTH; i++) {
+            printf("%02x",md[i]);
+    }
+    printf("\n");
+}
