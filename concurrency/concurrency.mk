8981a5c8df998dc77d5cf11d2749cfb8995803ac
Khalid Akash <khalkash@gmail.com>
2018-10-12 10:38:32 -0400

    Branch with tasksys.cpp as runtime task manager instead of custom task manager.

--- concurrency/concurrency.mk
+++ concurrency/concurrency.mk
@@ -3,7 +3,7 @@ GPP = g++
 
 CONC_DIR = ../concurrency
 
-CFLAGS = -g -I${CURDIR} -I${CURDIR}/..${CONC_DIR} -mavx
+CFLAGS = -g -I${CURDIR} -mavx
 LFLAGS = -pthread
 OPTFLAG = -O3
 
@@ -15,7 +15,11 @@ set_debug_opt_flag:
 	$(eval OPTFLAG = -O0)
 
 tasks.o: tasks.c
-	${GCC} ${OPTFLAG} ${CFLAGS} ${LFLAGS} -o tasks.o -c ../concurrency/tasks.c
+	${GCC} ${OPTFLAG} ${CFLAGS} ${LFLAGS} -o tasks.o -c tasks.c
+
+tasksys.o: tasksys.cpp
+	${GPP} ${OPTFLAG} ${CFLAGS} ${LFLAGS} -o tasksys.o -c tasksys.cpp
+
 
 clean: 
 	rm *.run *.o
